9a6d05cbcbaa2b0526bd6aeb28a9e033282e7122
Kyle Brett <kyle@kylebrett.com>
2013-06-24 18:44:10 -0700

    Add failing spec for board creation

--- lib/ttt/game.rb
+++ lib/ttt/game.rb
@@ -1,5 +1,7 @@
 module TTT
   class Game
+    SPACES = %w{a1 a2 a3 b1 b2 b3 c1 c2 c3}
+
     def initialize(dir)
       Dir.mkdir(dir)
     end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,5 +9,19 @@ describe TTT::Game do
         Dir.exists?(TEST_DIR)
       }.from(false).to(true)
     end
+
+    it 'knows the names of each space in Tic Tac Toe' do
+      # this is a dumb spec but whatever, typo check
+      TTT::Game::SPACES.should == %w{a1 a2 a3 b1 b2 b3 c1 c2 c3}
+    end
+
+    it 'should fill the created directory with a folder per board space' do
+      TTT::Game.new(TEST_DIR)
+      TTT::Game::SPACES.map do |space|
+        File.join(TEST_DIR, space)
+      end.each do |space|
+        Dir.exists?(space).should == true
+      end
+    end
   end
 end
